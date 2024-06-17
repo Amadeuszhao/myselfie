1898945050753778c12641e8d4f913bd68f3e80c
casperllm <casperllm@proton.me>
2024-06-17 16:02:33 +0800

    upload

---
+++ selfie/selfie_ori.py
@@ -0,0 +1,508 @@
+# %%
+from utils.utils import load_conversation_template
+template_name = 'llama-2'
+conv_template = load_conversation_template(template_name)
+
+# %%
+import fastchat
+print("This is version for fastchat",fastchat.__version__)
+print(conv_template)
+
+# %%
+from utils.modelUtils import *
+from utils.utils import *
+import seaborn as sns
+import torch
+import numpy as np
+import matplotlib.pyplot as plt
+from casper import nethook
+
+
+# %%
+model_name ="/root/autodl-tmp/repeat_output_harm_sys/checkpoint-40000"  # or "Llama2-7B" or "EleutherAI/gpt-neox-20b"
+mt = ModelAndTokenizer(
+    model_name,
+    torch_dtype=(torch.float16 if "20b" in model_name else None),
+    device = 'cuda'
+)
+mt.model.config
+
+# %%
+# import torch
+# from transformers import AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig
+# import argparse
+# import sys
+# # %%
+# from torch import nn
+# _CONFIG_FOR_DOC = "LlamaConfig"
+# from transformers.models.llama.modeling_llama import *
+# class LlamaForCausalLM_addin(LlamaPreTrainedModel):
+#     _tied_weights_keys = ["lm_head.weight"]
+
+#     def __init__(self, config):
+#         super().__init__(config)
+#         self.model = LlamaModel(config)
+#         self.vocab_size = config.vocab_size
+#         #self.extra_layer_1 =  nn.Linear(config.hidden_size, 11008, bias=False)
+#         #self.extra_layer_2 =  nn.Linear(11008, config.hidden_size,bias=False)
+#         self.extra_lm_head = nn.Linear(config.hidden_size * 2, config.vocab_size, bias=False)
+#         # self.lm_head = nn.Linear(config.hidden_size, config.vocab_size, bias=False)
+#         # Initialize weights and apply final processing
+#         self.post_init()
+
+#     def get_input_embeddings(self):
+#         return self.model.embed_tokens
+
+#     def set_input_embeddings(self, value):
+#         self.model.embed_tokens = value
+
+#     def get_output_embeddings(self):
+#         return self.lm_head
+
+#     def set_output_embeddings(self, new_embeddings):
+#         self.lm_head = new_embeddings
+
+#     def set_decoder(self, decoder):
+#         self.model = decoder
+
+#     def get_decoder(self):
+#         return self.model
+
+#     @add_start_docstrings_to_model_forward(LLAMA_INPUTS_DOCSTRING)
+#     @replace_return_docstrings(output_type=CausalLMOutputWithPast, config_class=_CONFIG_FOR_DOC)
+#     def forward(
+#         self,
+#         input_ids: torch.LongTensor = None,
+#         attention_mask: Optional[torch.Tensor] = None,
+#         position_ids: Optional[torch.LongTensor] = None,
+#         past_key_values: Optional[List[torch.FloatTensor]] = None,
+#         inputs_embeds: Optional[torch.FloatTensor] = None,
+#         labels: Optional[torch.LongTensor] = None,
+#         use_cache: Optional[bool] = None,
+#         output_attentions: Optional[bool] = None,
+#         output_hidden_states: Optional[bool] = None,
+#         return_dict: Optional[bool] = None,
+#         cache_position: Optional[torch.LongTensor] = None,
+#     ) -> Union[Tuple, CausalLMOutputWithPast]:
+#         r"""
+#         Args:
+#             labels (`torch.LongTensor` of shape `(batch_size, sequence_length)`, *optional*):
+#                 Labels for computing the masked language modeling loss. Indices should either be in `[0, ...,
+#                 config.vocab_size]` or -100 (see `input_ids` docstring). Tokens with indices set to `-100` are ignored
+#                 (masked), the loss is only computed for the tokens with labels in `[0, ..., config.vocab_size]`.
+
+#         Returns:
+
+#         Example:
+
+#         ```python
+#         >>> from transformers import AutoTokenizer, LlamaForCausalLM
+
+#         >>> model = LlamaForCausalLM.from_pretrained("meta-llama/Llama-2-7b-hf")
+#         >>> tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-2-7b-hf")
+
+#         >>> prompt = "Hey, are you conscious? Can you talk to me?"
+#         >>> inputs = tokenizer(prompt, return_tensors="pt")
+
+#         >>> # Generate
+#         >>> generate_ids = model.generate(inputs.input_ids, max_length=30)
+#         >>> tokenizer.batch_decode(generate_ids, skip_special_tokens=True, clean_up_tokenization_spaces=False)[0]
+#         "Hey, are you conscious? Can you talk to me?\nI'm not conscious, but I can talk to you."
+#         ```"""
+#         output_attentions = output_attentions if output_attentions is not None else self.config.output_attentions
+#         output_hidden_states = (
+#             output_hidden_states if output_hidden_states is not None else self.config.output_hidden_states
+#         )
+#         output_hidden_states = True
+#         return_dict = return_dict if return_dict is not None else self.config.use_return_dict
+
+#         # decoder outputs consists of (dec_features, layer_state, dec_hidden, dec_attn)
+#         outputs = self.model(
+#             input_ids=input_ids,
+#             attention_mask=attention_mask,
+#             position_ids=position_ids,
+#             past_key_values=past_key_values,
+#             inputs_embeds=inputs_embeds,
+#             use_cache=use_cache,
+#             output_attentions=output_attentions,
+#             output_hidden_states=output_hidden_states,
+#             return_dict=return_dict,
+#             cache_position=cache_position,
+#         )
+
+#         hidden_states = outputs[0]
+#         target_hidden_states = outputs.hidden_states[16]
+#         combine_hidden_states = torch.cat((hidden_states, target_hidden_states), dim=2)
+#         if self.config.pretraining_tp > 1:
+#             lm_head_slices = self.lm_head.weight.split(self.vocab_size // self.config.pretraining_tp, dim=0)
+#             logits = [F.linear(hidden_states, lm_head_slices[i]) for i in range(self.config.pretraining_tp)]
+#             logits = torch.cat(logits, dim=-1)
+#         else:
+#             # hidden_states = self.extra_layer_1(hidden_states)
+#             # hidden_states = self.extra_layer_2(hidden_states)
+            
+#             logits = self.extra_lm_head( combine_hidden_states)
+#             # logits = self.lm_head(hidden_states)
+
+#         logits = logits.float()
+
+#         loss = None
+#         if labels is not None:
+#             # Shift so that tokens < n predict n
+#             shift_logits = logits[..., :-1, :].contiguous()
+#             shift_labels = labels[..., 1:].contiguous()
+#             # Flatten the tokens
+#             loss_fct = CrossEntropyLoss()
+#             shift_logits = shift_logits.view(-1, self.config.vocab_size)
+#             shift_labels = shift_labels.view(-1)
+#             # Enable model parallelism
+#             shift_labels = shift_labels.to(shift_logits.device)
+#             loss = loss_fct(shift_logits, shift_labels)
+
+#         if not return_dict:
+#             output = (logits,) + outputs[1:]
+#             return (loss,) + output if loss is not None else output
+
+#         return CausalLMOutputWithPast(
+#             loss=loss,
+#             logits=logits,
+#             past_key_values=outputs.past_key_values,
+#             hidden_states=outputs.hidden_states,
+#             attentions=outputs.attentions,
+#         )
+
+#     def prepare_inputs_for_generation(
+#         self, input_ids, past_key_values=None, attention_mask=None, inputs_embeds=None, cache_position=None, **kwargs
+#     ):
+#         # With static cache, the `past_key_values` is None
+#         # TODO joao: standardize interface for the different Cache classes and remove of this if
+#         has_static_cache = False
+#         if past_key_values is None:
+#             past_key_values = getattr(getattr(self.model.layers[0], "self_attn", {}), "past_key_value", None)
+#             has_static_cache = past_key_values is not None
+
+#         past_length = 0
+#         if past_key_values is not None:
+#             if isinstance(past_key_values, Cache):
+#                 past_length = cache_position[0] if cache_position is not None else past_key_values.get_seq_length()
+#                 max_cache_length = (
+#                     torch.tensor(past_key_values.get_max_length(), device=input_ids.device)
+#                     if past_key_values.get_max_length() is not None
+#                     else None
+#                 )
+#                 cache_length = past_length if max_cache_length is None else torch.min(max_cache_length, past_length)
+#             # TODO joao: remove this `else` after `generate` prioritizes `Cache` objects
+#             else:
+#                 cache_length = past_length = past_key_values[0][0].shape[2]
+#                 max_cache_length = None
+
+#             # Keep only the unprocessed tokens:
+#             # 1 - If the length of the attention_mask exceeds the length of input_ids, then we are in a setting where
+#             # some of the inputs are exclusively passed as part of the cache (e.g. when passing input_embeds as
+#             # input)
+#             if attention_mask is not None and attention_mask.shape[1] > input_ids.shape[1]:
+#                 input_ids = input_ids[:, -(attention_mask.shape[1] - past_length) :]
+#             # 2 - If the past_length is smaller than input_ids', then input_ids holds all input tokens. We can discard
+#             # input_ids based on the past_length.
+#             elif past_length < input_ids.shape[1]:
+#                 input_ids = input_ids[:, past_length:]
+#             # 3 - Otherwise (past_length >= input_ids.shape[1]), let's assume input_ids only has unprocessed tokens.
+
+#             # If we are about to go beyond the maximum cache length, we need to crop the input attention mask.
+#             if (
+#                 max_cache_length is not None
+#                 and attention_mask is not None
+#                 and cache_length + input_ids.shape[1] > max_cache_length
+#             ):
+#                 attention_mask = attention_mask[:, -max_cache_length:]
+
+#         position_ids = kwargs.get("position_ids", None)
+#         if attention_mask is not None and position_ids is None:
+#             # create position_ids on the fly for batch generation
+#             position_ids = attention_mask.long().cumsum(-1) - 1
+#             position_ids.masked_fill_(attention_mask == 0, 1)
+#             if past_key_values:
+#                 position_ids = position_ids[:, -input_ids.shape[1] :]
+
+#         # if `inputs_embeds` are passed, we only want to use them in the 1st generation step
+#         if inputs_embeds is not None and past_key_values is None:
+#             model_inputs = {"inputs_embeds": inputs_embeds}
+#         else:
+#             # The `contiguous()` here is necessary to have a static stride during decoding. torchdynamo otherwise
+#             # recompiles graphs as the stride of the inputs is a guard. Ref: https://github.com/huggingface/transformers/pull/29114
+#             # TODO: use `next_tokens` directly instead.
+#             model_inputs = {"input_ids": input_ids.contiguous()}
+
+#         input_length = position_ids.shape[-1] if position_ids is not None else input_ids.shape[-1]
+#         if cache_position is None:
+#             cache_position = torch.arange(past_length, past_length + input_length, device=input_ids.device)
+#         else:
+#             cache_position = cache_position[-input_length:]
+
+#         if has_static_cache:
+#             past_key_values = None
+
+#         model_inputs.update(
+#             {
+#                 "position_ids": position_ids,
+#                 "cache_position": cache_position,
+#                 "past_key_values": past_key_values,
+#                 "use_cache": kwargs.get("use_cache"),
+#                 "attention_mask": attention_mask,
+#             }
+#         )
+#         return model_inputs
+
+#     @staticmethod
+#     def _reorder_cache(past_key_values, beam_idx):
+#         reordered_past = ()
+#         for layer_past in past_key_values:
+#             reordered_past += (
+#                 tuple(past_state.index_select(0, beam_idx.to(past_state.device)) for past_state in layer_past),
+#             )
+#         return reordered_past
+
+# # %%
+# def print_trainable_parameters(model):
+#     """
+#         Prints the number of trainable parameters in the model.
+#     """
+#     trainable_params = 0
+#     all_param = 0
+        
+#     for _, param in model.named_parameters():
+#         all_param += param.numel()
+#         if param.requires_grad:
+#             trainable_params += param.numel()
+        
+#     print(
+#             f"trainable params: {trainable_params} || all params: {all_param} || trainable%: {100 * trainable_params / all_param}"
+#     )
+# def generate_prompt(data_point):
+#     """
+#     Generate input text based on a prompt, task instruction, (context info.), and answer
+
+#     :param data_point: dict: Data point
+#     :return: dict: tokenized prompt
+#     """
+#     if "[INST]" not in  data_point["text"]:
+#         text = '[INST] ' + data_point["text"]
+#     else:
+#         text =  data_point["text"]
+
+#     return text
+
+
+# %%
+# from transformers.models.llama.modeling_llama import *
+
+# # %%
+# model_id = "/root/autodl-tmp/repeat_output_combine/checkpoint-50000"
+# tokenizer_id = "/root/autodl-tmp/large/model"
+# model = LlamaForCausalLM_addin.from_pretrained(model_id).cuda()
+# # model = LlamaForCausalLM.from_pretrained(model_id, device_map="auto")
+
+# tokenizer =  AutoTokenizer.from_pretrained(tokenizer_id)
+
+# # %%
+# model
+
+# %%
+# mt.model = model
+
+# %%
+mt.model
+
+# %%
+pad_token_id = mt.tokenizer.pad_token_id
+pad_token_id 
+
+# %%
+conv_template_repeat = load_conversation_template(template_name)
+conv_template_repeat.system_message =  "You are a repeating assistant, you only need to repeat the follow prompt and don't need to answer it"
+
+# %%
+prompt = "Tell me a joke"
+generate_outputs(generate_input(conv_template,prompt) ,mt)
+
+# %%
+prompt = "Tell me a joke"
+generate_outputs(generate_input(conv_template_repeat,prompt) ,mt)
+
+# %%
+def replace_elements(arr, start, end, new_elements):
+    """
+    Replace elements in an array from 'start' index to 'end' index with 'new_elements'.
+
+    Args:
+    - arr (list or numpy.ndarray): The original array.
+    - start (int): The starting index for replacement (inclusive).
+    - end (int): The ending index for replacement (exclusive).
+    - new_elements (list or numpy.ndarray): The new elements to insert in place of the old ones.
+
+    Returns:
+    - numpy.ndarray: The modified array with replaced elements.
+    """
+    # Ensure input is a NumPy array
+    if not isinstance(arr, np.ndarray):
+        arr = np.array(arr)
+    
+    if not isinstance(new_elements, np.ndarray):
+        new_elements = np.array(new_elements)
+    
+    # Create a new array with the replacement
+    new_arr = np.concatenate([arr[:start], new_elements, arr[end:]])
+    
+    return new_arr
+
+# %%
+prompt = "Tell me a joke"
+# 对于rep的prompt是[39:-5]之间是异常的值
+# 对于no_input是[4:-6]
+def generate_padd_tokens(prompt,length=None):
+    inp = generate_input(conv_template_repeat,prompt)
+    inp = mt.tokenizer(inp,return_tensors="pt")
+    tokens = inp['input_ids']
+    attention_mask = inp['attention_mask']
+    print(mt.tokenizer.decode(tokens[0][39:-6]))
+    if length == None:
+        tokens[:, 39:-5] = 2
+    else:
+        tokens = tokens.numpy()
+        tokens = replace_elements(tokens[0],39,-5,[2] * length)
+        tokens = torch.tensor(tokens)
+        attention_mask = torch.tensor([1] * len(tokens))
+        tokens = tokens.unsqueeze(0)
+        attention_mask = attention_mask.unsqueeze(0)
+    tokens = tokens.cuda()
+    attention_mask = attention_mask.cuda()
+    return tokens, attention_mask
+
+# %%
+def generate_normal(prompt):
+    inp = generate_input(conv_template,prompt)
+    inp = mt.tokenizer(inp,return_tensors="pt")
+    tokens = inp['input_ids']
+    attention_mask = inp['attention_mask']
+    # print(mt.tokenizer.decode(tokens[0][4:-6]))
+    tokens = tokens.cuda()
+    attention_mask = attention_mask.cuda()
+    return tokens, attention_mask
+
+# %%
+def generate_response(token, attn, model,tokenizer, max_new_tokens=100):
+    num_input_tokens = token.shape[1]
+    # print(tokenizer.decode(input_ids['input_ids'].squeeze(0)))
+    outputs = model.generate(token, attention_mask=attn,
+                                     max_new_tokens=max_new_tokens, do_sample=False, pad_token_id=tokenizer.pad_token_id)
+    generation = tokenizer.batch_decode(outputs[:, num_input_tokens:], skip_special_tokens=True)
+    return generation
+
+# %%
+def hidden_states_decoder(
+    model,  # The model
+    hidden_state,
+    layers,
+    tokens,
+    attn,
+):    
+    prng = np.random.RandomState(1)  # For reproducibility, use pseudorandom noise
+    flag = True
+
+    def untuple(x):
+        return x[0] if isinstance(x, tuple) else x
+
+    def patch_rep(x, layer):
+        nonlocal flag
+        if layer not in layers:
+            return x
+
+        if layer == layers[0] and flag:
+            
+            inter_states = x[0].cpu()
+            atten_mask = x[1][0][0].cpu()
+            posi_ids = x[1][0][1].cpu()
+            print(inter_states.shape)
+            flag = False
+            inter_states[:, 39:-6] = hidden_state
+            inter_states = inter_states.cuda()
+            print(inter_states.shape)
+            short_cut_2 = (atten_mask ,posi_ids)
+            short_cut = (inter_states , short_cut_2)
+            
+            return  short_cut 
+    with torch.no_grad(), nethook.TraceDict(
+        model,
+        layers,
+        edit_output=patch_rep,
+    ) as td:
+        output = generate_response(token, attn, model,mt.tokenizer, max_new_tokens=100)
+        
+    return output
+
+# %%
+import json
+template =  '''[INST] {instruction} [/INST] '''
+with open("/root/autodl-tmp/repe/outputs_short.json",'r') as f:
+    outputs = json.load(f)
+outputs
+
+# %%
+harmfuls = []
+for t in outputs:
+    harmfuls.append(template.format(instruction = t['behavior']))
+harmfuls
+
+
+# %%
+import json
+
+# for idx in range(405):
+#     save_hidden_states = []
+#     for i in range(32):
+#         temp = np.load(f"/root/autodl-tmp/repe/short_hidden_states/harmful_{idx}/harmful_hidden_state{i}.npy")
+#         temp = torch.tensor(temp)
+#         length = temp.size()[1]
+#         # print(length)
+#         save_hidden_states.append(temp)
+#     outputs = []
+#     for layer in range(31):
+#         token, attn =  generate_padd_tokens(harmfuls[idx],length + 1)
+#         print(token.shape)
+#         layers = [layername(mt.model, 0)]
+#         output = hidden_states_decoder(mt.model, save_hidden_states[layer],  layers, token, attn)
+#         outputs.append(output)
+#     with open(f"/root/autodl-tmp/myselfie/short_results_original/{idx}.json",'w') as f:
+#         json.dump(outputs,f,indent =4)
+
+# %%
+import json
+
+for idx in range(405):
+    save_hidden_states = []
+    for i in range(32):
+        temp = np.load(f"/root/autodl-tmp/repe/short_hidden_states/harmful_{idx}/harmful_hidden_state{i}.npy")
+        temp = torch.tensor(temp)
+        length = temp.size()[1]
+        #print(length)
+        save_hidden_states.append(temp)
+    outputs = []
+    for layer in range(31):
+        token, attn =  generate_padd_tokens(harmfuls[idx],length + 1)
+        #print(token.shape)
+        layers = [layername(mt.model, layer)]
+        output = hidden_states_decoder(mt.model, save_hidden_states[layer],  layers, token, attn)
+        outputs.append(output)
+    with open(f"/root/autodl-tmp/myselfie/short_results_original/layer_{idx}.json",'w') as f:
+        json.dump(outputs,f,indent =4)
+
+# %%
+outputs
+
+# %%
+
+
+# %%
+
+
+

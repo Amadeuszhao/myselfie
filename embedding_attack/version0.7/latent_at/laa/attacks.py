0e656e8b975f74fb635fcdbc2a12768f525a91bb
赵为 <lixy0311@163.com>
2024-08-12 14:35:15 +0800

    upload

---
+++ embedding_attack/version0.7/latent_at/laa/attacks.py
@@ -0,0 +1,319 @@
+import torch
+import einops
+import numpy as np
+import os
+from torch import nn
+import torch.nn.functional as F
+
+# The attacker is parameterized by a low-rank MLP (not used by default)
+class LowRankAdversary(torch.nn.Module):
+    
+    def __init__(self, dim, rank, device, bias=False, zero_init=True):
+        super().__init__()
+        self.dim = dim
+        self.rank = rank
+        self.device = device
+        self.lora_A = torch.nn.Linear(dim, rank, bias=False).to(device)
+        self.lora_B = torch.nn.Linear(rank, dim, bias=bias).to(device)
+        if zero_init:
+            self.lora_B.weight.data.zero_()
+    
+    def forward(self, x):
+        return self.lora_B(self.lora_A(x)) + x
+
+
+# The attacker is parameterized by a full-rank MLP (not used by default)
+class FullRankAdversary(torch.nn.Module):
+    
+    def __init__(self, dim, device, bias=False):
+        super().__init__()
+        self.dim = dim
+        self.device = device
+        self.m = torch.nn.Linear(dim, dim, bias=bias).to(device)
+
+        self.m.weight.data.zero_()
+    
+    def forward(self, x):
+        return self.m(x) + x
+def get_next_available_path(base_path, base_name, extension=""):
+    """
+    获取下一个可用的文件或目录路径。
+    
+    :param base_path: 文件或目录的基础路径
+    :param base_name: 文件或目录的基础名称
+    :param extension: 文件的扩展名（如果是文件）
+    :return: 下一个可用的文件或目录路径
+    """
+    index = 1
+    while True:
+        path = os.path.join(base_path, f"{base_name}_{index}{extension}.npy")
+        # print(path)
+        if not os.path.exists(path):
+            return path
+        index += 1
+
+def get_new_path():
+    base_path = "/root/autodl-tmp/at/uap"
+    base_name = "temp_attack"
+    new_path = get_next_available_path(base_path, base_name)
+    return new_path
+def compute_toward_away_loss_adversary(
+    model,
+    towards_tokens,
+    towards_labels_mask,
+    away_tokens,
+    away_labels_mask,
+    towards_labels=None,
+    away_labels=None,
+    coefs={"toward": 1.0, "away": 1.0},
+    accelerator=None,
+):
+    losses = {"total": 0}
+
+    if towards_tokens is not None:
+        with torch.no_grad():
+            logits = model(input_ids=towards_tokens.cuda()).logits
+            final_logits = logits[:, :-1][towards_labels_mask[:, 1:]]
+            if towards_labels is None:
+                towards_labels = towards_tokens[:, 1:][towards_labels_mask[:, 1:]].cuda()
+
+            toward_loss = F.cross_entropy(final_logits, towards_labels)
+            losses["toward"] = toward_loss.item()
+            losses["total"] += toward_loss.item()
+
+    if away_tokens is not None:
+        with torch.no_grad():
+            logits = model(input_ids=away_tokens.cuda()).logits
+            final_logits = logits[:, :-1][away_labels_mask[:, 1:]]
+            if away_labels is None:
+                away_labels = away_tokens[:, 1:][away_labels_mask[:, 1:]].cuda()
+            away_loss = F.cross_entropy(F.log_softmax(final_logits, dim=-1), away_labels)
+
+            losses["away"] = away_loss.item()
+            losses["total"] += away_loss.item()
+
+    return losses
+
+# Standard projected gradient attack (used by default)
+class GDAdversary(torch.nn.Module):
+    
+    def __init__(self, dim, epsilon, attack_mask, batch,embedding_space=None, device=None, dtype=None):
+        super().__init__()
+        self.device = device
+        self.epsilon = epsilon
+        self.batch = batch
+        self.attack_mask = attack_mask
+
+        if dtype:
+            self.attack = torch.nn.Parameter(torch.zeros(
+                attack_mask.shape[0], attack_mask.shape[1], dim, device=self.device, dtype=dtype))
+        else:
+            self.attack = torch.nn.Parameter(torch.zeros(
+                attack_mask.shape[0], attack_mask.shape[1], dim, device=self.device))
+        self.embedding_space = torch.tensor(embedding_space, device=self.device, dtype=self.attack.dtype)
+        random_indices = torch.randint(0, self.embedding_space.shape[0], (1, attack_mask.shape[1]))
+        initial_attack = self.embedding_space[random_indices].to(device)
+        self.attack = torch.nn.Parameter(initial_attack) 
+        # print("This is the shape for GDadversary", self.attack.shape)
+        # uap_path = "/root/autodl-tmp/at/uap/temp_attack.npy"
+        # if os.path.exists(uap_path):
+        #     #print('load in uap')
+        #     #print(self.attack_mask)
+        #     uap_tensor = torch.tensor(np.load(uap_path), device=self.device, dtype=self.attack.dtype)
+        #     # Create a new tensor with the required values
+        #     new_attack = self.attack.clone()
+        #     new_attack[self.attack_mask] = uap_tensor
+            
+        #     # Assign the new tensor to self.attack to avoid in-place operation
+        #     self.attack = torch.nn.Parameter(new_attack)    
+
+        self.clip_attack()
+        
+    def forward(self, x):
+        #print(x.shape)
+        if x.shape[1] == 1 and self.attack.shape[1] != 1:  # generation mode (perturbation already applied)
+            #print("pass")
+            return x
+        else:
+            #np.save('x2', x.to(torch.float32).detach().cpu().numpy())
+            if self.device is None or self.device != x.device:
+                with torch.no_grad():
+                    self.device = x.device
+                    self.attack.data = self.attack.data.to(self.device)
+                    self.attack_mask = self.attack_mask.to(self.device)
+            #print("This is x shape", x.shape)
+            #print(x.shape[1])
+            #print("This is attack mask shape",self.attack_mask.shape)
+            if x.shape[1] == self.attack_mask.shape[1]: # training model
+                perturbed_acts =  self.attack[self.attack_mask[:, :x.shape[1]]].to(x.dtype)
+                x[self.attack_mask[:, :x.shape[1]]] = perturbed_acts
+            elif x.shape[1] != self.attack_mask.shape[1] and x.shape[1] != 1: # evaluation model
+                print("run only on suffix")
+                temp_mask = self.attack_mask[:, :x.shape[1]]
+                temp_attack = self.attack[:, :x.shape[1], :]
+                np.save('/root/autodl-tmp/at/uap/temp_attack', temp_attack[temp_mask[:, :x.shape[1]]].to(torch.float32).detach().cpu().numpy())
+                temp_path = get_new_path()
+                np.save(temp_path, temp_attack[temp_mask[:, :x.shape[1]]].to(torch.float32).detach().cpu().numpy())
+                perturbed_acts =  temp_attack[temp_mask[:, :x.shape[1]]].to(x.dtype)
+                #print(temp_mask[:, :x.shape[1]])
+                x[temp_mask[:, :x.shape[1]]] = perturbed_acts
+                #np.save('perturb_x2', x.to(torch.float32).detach().cpu().numpy())
+            #print("This is the part that we changed")
+            #print(self.attack_mask[:, :x.shape[1]])
+            return x
+    def save_uap(self, path):
+        print("save uap")
+        temp_mask = self.attack_mask[:, :]
+        temp_attack = self.attack[:, :, :]
+        np.save(path, self.attack[self.attack_mask[:, :]].to(torch.float32).detach().cpu().numpy())
+    
+    def calculate_top_k_embedding_loss(self, k=10):
+        loss = 0.0
+        for i in range(self.attack.shape[1]):
+            # 计算 self.attack 与 embedding_space 中最相似的前 k 个向量的距离
+            distances = torch.norm(self.embedding_space - self.attack[0, i], dim=-1)
+            top_k_indices = torch.topk(distances, k, largest=False).indices
+            top_k_embeddings = self.embedding_space[top_k_indices]
+            
+            # 计算目标向量与 top_k_embeddings 的余弦相似度损失
+            cosine_similarity = nn.CosineSimilarity(dim=-1)
+            similarity_scores = cosine_similarity(self.attack[0, i].unsqueeze(0), top_k_embeddings)
+            
+            # 最小化相似度的负值（即最大化相似度）
+            top_k_loss = -torch.mean(similarity_scores)
+            loss += top_k_loss    
+        return loss
+
+    def calculate_embedding_loss(self):
+        loss = 0.0
+        for i in range(self.attack.shape[1]):
+            # 计算 self.attack 与 embedding_space 中所有向量的余弦相似度
+            cosine_similarity = nn.CosineSimilarity(dim=-1)
+            similarity_scores = cosine_similarity(self.attack[0, i].unsqueeze(0), self.embedding_space)
+            
+            # 最小化相似度的负值（即最大化相似度）
+            total_loss = -torch.mean(similarity_scores)
+            loss += total_loss    
+        return loss
+    
+    def _compute_losses(self, candidate_embeddings,model,index):
+        original_attack = self.attack.detach().clone()
+        self.attack[0, index] = candidate_embeddings.to(self.device)
+        with torch.no_grad():
+            losses = compute_toward_away_loss_adversary(
+                model,
+                self.batch["adv_padd_prompt_tokens"],
+                self.batch["adv_padd_labels_mask"],
+                self.batch["def_padd_prompt_tokens"],
+                self.batch["def_padd_labels_mask"],
+                self.batch.get("adv_labels"),
+                self.batch.get("def_labels"),
+                {"toward": 1.0, "away": 1.0},
+            )
+            total_loss = losses["total"]
+
+        self.attack[0] = original_attack
+        del original_attack
+
+        return total_loss
+
+    def select_best_embedding(self, k=10, model=None):
+        with torch.no_grad():
+            best_embeddings = self.attack.clone()
+            attack_indices = self.attack_mask[0].nonzero(as_tuple=True)[0]
+            for i in attack_indices:
+                print(i)
+                distances = torch.norm(self.embedding_space - self.attack[0, i], dim=-1)
+                top_k_indices = torch.topk(distances, k, largest=False).indices
+                top_k_embeddings = self.embedding_space[top_k_indices]
+                # 计算每个候选嵌入的总损失
+                total_losses = torch.tensor([
+                    self._compute_losses(candidate.unsqueeze(0), model=model, index = i) for candidate in top_k_embeddings
+                ], device=self.device)
+                
+                best_idx = torch.argmin(total_losses)
+                best_embeddings[0, i] = top_k_embeddings[best_idx]
+                del distances, top_k_indices, top_k_embeddings, total_losses
+            
+            self.attack.copy_(best_embeddings)
+            del best_embeddings
+
+    def compute_away_loss(self,model):
+        away_tokens = self.batch["def_tokens"].to(self.device)
+        away_labels_mask = self.batch["def_labels_mask"].to(self.device)
+        logits = model(input_ids=away_tokens).logits
+        final_logits = logits[:, :-1][away_labels_mask[:, 1:].to(self.device)]
+        away_labels = away_tokens[:, 1:][away_labels_mask[:, 1:].to(self.device)]
+        loss = nn.CrossEntropyLoss()(final_logits, away_labels)
+        del final_logits, away_labels, logits
+        return loss.item()
+
+    def compute_toward_loss(self, model):
+        toward_tokens = self.batch["adv_tokens"].to(self.device)
+        toward_labels_mask = self.batch["adv_labels_mask"].to(self.device)
+        logits = model(input_ids=toward_tokens).logits
+        final_logits = logits[:, :-1][toward_labels_mask[:, 1:].to(self.device)]
+        toward_labels = toward_tokens[:, 1:][toward_labels_mask[:, 1:].to(self.device)]
+        loss = nn.CrossEntropyLoss()(final_logits, toward_labels)
+        del final_logits, toward_labels, logits
+
+        return loss.item()
+
+    def clip_attack(self,select_best=False, model=None):
+        if select_best and model != None:
+            print("new clip attack")
+            self.select_best_embedding(k=10, model=model)
+        else:
+            if self.embedding_space == None:
+                with torch.no_grad():
+                    # clip attack norm to eps
+                    norms = torch.norm(self.attack, dim=-1, keepdim=True)
+                    scale = torch.clamp(norms / self.epsilon, min=1)
+                    self.attack.div_(scale)
+
+                    norms = torch.norm(self.attack, dim=-1)
+            else:
+                print("start clipping")
+                with torch.no_grad():
+                # 将 self.attack 的值限制为 embedding_space 中的值
+                    closest_embeddings = torch.zeros_like(self.attack)
+                    for i in range(self.attack.shape[1]):
+                        distances = torch.norm(self.embedding_space - self.attack[0, i], dim=-1)
+                        closest_idx = torch.argmin(distances)
+                        closest_embeddings[0, i] = self.embedding_space[closest_idx]
+                    self.attack.copy_(closest_embeddings)
+
+# Whitened adversaries train perturbations in a whitened space (not used by default)
+class WhitenedGDAdversary(torch.nn.Module):
+
+    def __init__(self, dim, device, epsilon, attack_mask, proj=None, inv_proj=None):
+        super().__init__()
+        self.attack = None
+        self.device = device
+        self.epsilon = epsilon
+        self.proj = proj  # proj is a projection matrix (e.g. one obtained using PCA) to whiten the attack
+
+        if inv_proj is None:
+            self.inv_proj = torch.inverse(proj)
+        else:
+            self.inv_proj = inv_proj
+
+        self.attack = torch.nn.Parameter(torch.randn(
+            attack_mask.shape[0], attack_mask.shape[1], dim, device=self.device))
+        self.clip_attack()
+        self.attack_mask = attack_mask
+    
+    def forward(self, x):
+        unprojected_attack = torch.einsum("n d, batch seq n-> batch seq d", self.inv_proj, self.attack) # n is whitened dimension, d is original hidden size (technically same here)
+        x[self.attack_mask[:, :x.shape[1]]] = (x + unprojected_attack.to(x.dtype))[self.attack_mask[:, :x.shape[1]]]
+        return x
+
+    
+    def clip_attack(self):
+        with torch.no_grad():
+            # clip attack norm to eps
+            norms = torch.norm(self.attack, dim=-1, keepdim=True)
+            scale = torch.clamp(norms / self.epsilon, min=1)
+            self.attack.div_(scale)
+
+            norms = torch.norm(self.attack, dim=-1)
---
+++ embedding_attack/version0.8/test.py
@@ -0,0 +1,327 @@
+# %% [markdown]
+# # Latent Space Adversarial Attack
+# 
+# This notebook demonstrates conducting a latent space adversarial attacks on LLMs. These particualr demo attacks are created using projected gradient descent to make a model more jailbreakable. 
+# 
+# ## Imports
+
+# %%
+import time
+import json
+import os
+import torch
+import sys
+from dotenv import load_dotenv
+from torch.utils.data import DataLoader
+from transformers import AutoModelForCausalLM, AutoTokenizer
+from tqdm import tqdm
+os.chdir("../")
+cwd = os.getcwd()
+if cwd not in sys.path:
+    sys.path.insert(0, cwd)
+from latent_at import *
+from tasks.harmbench.HarmBenchTask import HarmBenchTask
+import torch
+import numpy as np
+import random
+import logging
+def setup_logger(log_directory, log_filename):
+    # Create the log directory if it doesn't exist
+    if not os.path.exists(log_directory):
+        os.makedirs(log_directory)
+
+    # Set up the logger
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.DEBUG)
+
+    # Create a file handler
+    log_path = os.path.join(log_directory, log_filename)
+    file_handler = logging.FileHandler(log_path)
+    file_handler.setLevel(logging.DEBUG)
+
+    # Create a logging format
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+    file_handler.setFormatter(formatter)
+
+    # Add the handlers to the logger
+    logger.addHandler(file_handler)
+
+    return logger
+
+# Usage
+log_dir = '/root/autodl-tmp/at/logs'
+log_file = 'experiment_on_8.11_500.log'
+logger = setup_logger(log_dir, log_file)
+
+def set_seed(seed):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed(seed)
+        torch.cuda.manual_seed_all(seed)  # if you are using multi-GPU.
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+
+set_seed(42)
+load_dotenv()
+hf_access_token = os.getenv("HUGGINGFACE_API_KEY")
+
+# %%
+## Configuration
+
+# Set whether to use Llama2-7B or Llama3-8B.
+
+# %%
+use_llama2 = True
+if use_llama2:  # use llama2-7b
+    model_name = "/root/autodl-tmp/base/model"
+else: # use llama3-8b
+    model_name = "meta-llama/Meta-Llama-3-8B-Instruct"
+
+# %% [markdown]
+# ## Model
+
+# %%
+model_dtype = torch.float32
+device = "cuda"
+run_start_evals = False
+
+model = AutoModelForCausalLM.from_pretrained(
+    model_name,
+    token=hf_access_token,
+    torch_dtype=model_dtype
+).to(device)
+
+
+if "Llama-2" or 'base' in model_name:
+    model_type = "llama2"
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    tokenizer.pad_token_id = tokenizer.eos_token_id
+    tokenizer.padding_side = "left"
+elif "Llama-3" in model_name:
+    model_type = "llama3"
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    tokenizer.pad_token_id = tokenizer.eos_token_id
+    tokenizer.padding_side = "left"
+elif "zephyr" in model_name or "mistral" in model_name:
+    model_type = "zephyr"    
+    tokenizer = AutoTokenizer.from_pretrained("HuggingFaceH4/zephyr-7b-beta")
+    tokenizer.pad_token_id = tokenizer.unk_token_id
+    tokenizer.padding_side = "left"
+else:
+    print(model_name)
+    raise Exception("Unsupported model type?")
+
+# %% [markdown]
+# ## Data
+
+# %%
+advbench_data = HarmBenchTask(
+    tokenizer=tokenizer,
+    gen_batch_size=1,
+    cls_batch_size=1,
+    device=device,
+    data_name="advbench",
+    train_test_split=.8
+)
+
+harmbench_data = HarmBenchTask(
+    tokenizer=tokenizer,
+    gen_batch_size=1,
+    cls_batch_size=1,
+    device=device,
+    data_name="harmbench_text",
+    train_test_split=.8,
+    func_categories=["standard", "contextual"]
+)
+
+# train_behaviors = advbench_data.train_behaviors + harmbench_data.train_behaviors  
+
+sys_prompt = """You are a helpful, respectful and honest assistant. Always answer as helpfully as possible, while being safe. Your answers should not include any harmful, unethical, racist, sexist, toxic, dangerous, or illegal content. Please ensure that your responses are socially unbiased and positive in nature.
+
+If a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. If you don't know the answer to a question, please don't share false information."""
+
+if model_type == "llama2":
+    use_tokenizer_template = False
+    custom_prompt_template = "[INST] {prompt} [/INST] \n"
+    custom_completion_template = "{completion}"
+elif model_type == "llama3":
+    use_tokenizer_template = False
+    custom_prompt_template = "<|start_header_id|>user<|end_header_id|\n\n{prompt}<|eot_id|><|start_header_id|>assistant<|end_header_id|>\n\n"
+    custom_completion_template="{completion}"
+else:
+    sys_prompt=""
+    use_tokenizer_template = False
+    custom_prompt_template = "<|user|>\n{prompt}</s> \n <|assistant|>\n"
+    custom_completion_template="{completion}"
+ 
+lat_dataset = process_generic_chat_dataset(
+    tokenizer,
+    dataset="LLM-LAT/harmful-dataset",
+    adv_column="rejected",
+    def_column="chosen",
+    split="train",
+    use_tokenizer_template=use_tokenizer_template,
+    system_prompt="",
+    custom_prompt_template=custom_prompt_template,
+    custom_completion_template=custom_completion_template
+)
+
+lat_dataloader = DataLoader(
+    lat_dataset,
+    batch_size=1,
+    shuffle=True,
+    drop_last=True,
+    collate_fn=LatentAdversarialTrainingDataCollator(
+        tokenizer.pad_token_id,
+        truncate_length=2048
+    )
+)
+
+dataloader = iter(lat_dataloader)
+
+# %%
+batch = next(dataloader)
+import os
+import shutil
+
+# 文件夹路径
+folder_path = '/root/autodl-tmp/at/uap'
+
+# 检测文件夹是否存在
+if os.path.exists(folder_path):
+    # 如果文件夹存在，删除文件夹
+    shutil.rmtree(folder_path)
+
+# 创建新的文件夹
+os.makedirs(folder_path)
+
+print(f'文件夹 {folder_path} 已创建。')
+# %%
+tokenizer.decode(batch['adv_padd_prompt_tokens'][0][40-20:40])
+
+# %%
+adv_padd = batch['adv_padd_prompt_tokens']
+def_padd = batch['def_padd_prompt_tokens']
+prompt_mask = batch['padd_prompt_mask']
+def_padd_mask = batch['adv_padd_labels_mask']
+adv_padd_mask = batch['adv_padd_labels_mask']
+print(tokenizer.decode(adv_padd[adv_padd_mask]))
+tokenizer.decode(def_padd[def_padd_mask])
+embedding_space = np.load("/root/autodl-tmp/myselfie/embeddings.npy")
+print("This is shape for embedding", embedding_space.shape)
+# %%
+data = []
+
+
+# %% [markdown]
+# ## Clean Performance
+
+# %% [markdown]
+# ## Latent Space Attack
+start_time = time.time()
+model.eval()
+def freeze_parameters(model):
+    for param in model.parameters():
+        param.requires_grad = False
+freeze_parameters(model)
+# %%
+losses, wrappers = projected_gradient_descent_onpadd(
+    batch=batch,  # batch
+    model=model,  # model
+    model_layers_module="model.layers",  # where the model layers are
+    layer=["embedding", ],  # layers to attack
+    epsilon=0.5496,  # attack l2 constraint
+    l2_regularization=0.0,  # coef for l2 penalty on the attack
+    learning_rate=2e-3,  # attack step size
+    pgd_iterations=500,  # how many steps of projected gradient descent to do
+    loss_coefs={"toward": 0.8, "away": 0.5,},  # coefficients for the attack's toward and away losses
+    log_loss=True,  # whether to use a log loss instead of a crossentropy one
+    return_loss_over_time=True,
+    device="cuda",
+    embedding_constraint=False,
+    embedding_method = 'model',
+    embedding_space=embedding_space,
+    tokenizer = tokenizer,
+    logger = logger
+)
+end_time = time.time()
+execution_time = end_time - start_time
+print(f"代码执行时间为: {execution_time} 秒")
+print("***ADVERSARIAL LOSSES OVER TIME***\n")
+print([round(l['adv_total'], 4) for l in losses])
+
+# %% [markdown]
+# ## Attacked Performance
+
+# %%
+for wrapper in wrappers:
+    wrapper.enabled = True  # the wrappers should already be enabled, so this is redundant
+with torch.no_grad():
+    outputs = model.generate(
+        batch["adv_padd_prompt_tokens"][0][0:batch["prompt_length"][0][2]].unsqueeze(0).to("cuda"),
+        max_length=batch["adv_padd_prompt_tokens"].shape[1] + 200,
+        do_sample=False
+    )
+
+print("***ATTACKED PERFORMANCE***\n")
+prompt = tokenizer.decode( batch["adv_padd_prompt_tokens"][0][0:batch["prompt_length"][0][2]]).replace('\n', '')
+print("Prompt:\n" + prompt + "\n")
+prompt_response = tokenizer.decode(outputs[0]).replace('\n', '')
+print("Completion:\n" + prompt_response[len(prompt):])
+data.append({
+            'losses': [round(l['adv_total'], 4) for l in losses],
+            'prompt': prompt,
+            'prompt_response': prompt_response[len(prompt):]
+        })
+with open('/root/autodl-tmp/at/output.json', 'w', encoding='utf-8') as f:
+    json.dump(data, f, ensure_ascii=False, indent=4)
+
+for batch in dataloader:
+    losses, wrappers = projected_gradient_descent_onpadd(
+    logger=logger,
+    batch=batch,  # batch
+    model=model,  # model
+    model_layers_module="model.layers",  # where the model layers are
+    layer=["embedding", ],  # layers to attack
+    epsilon=0.5496,  # attack l2 constraint
+    l2_regularization=0.0,  # coef for l2 penalty on the attack
+    learning_rate=2e-3,  # attack step size
+    pgd_iterations=500,  # how many steps of projected gradient descent to do
+    loss_coefs={"toward": 0.8, "away": 0.5,},  # coefficients for the attack's toward and away losses
+    log_loss=True,  # whether to use a log loss instead of a crossentropy one
+    return_loss_over_time=True,
+    device="cuda",
+    embedding_constraint=False,
+    embedding_method = 'model',
+    embedding_space=embedding_space,
+    tokenizer = tokenizer,
+)
+
+    print("***ADVERSARIAL LOSSES OVER TIME***\n")
+    print([round(l['adv_total'], 4) for l in losses])
+    for wrapper in wrappers:
+        wrapper.enabled = True  # the wrappers should already be enabled, so this is redundant
+    with torch.no_grad():
+        outputs = model.generate(
+            batch["adv_padd_prompt_tokens"][0][0:batch["prompt_length"][0][2]].unsqueeze(0).to("cuda"),
+            max_length=batch["adv_padd_prompt_tokens"].shape[1] + 200,
+            do_sample=False
+        )
+
+    print("***ATTACKED PERFORMANCE***\n")
+    prompt = tokenizer.decode( batch["adv_padd_prompt_tokens"][0][0:batch["prompt_length"][0][2]]).replace('\n', '')
+    print("Prompt:\n" + prompt + "\n")
+    prompt_response = tokenizer.decode(outputs[0]).replace('\n', '')
+    print("Completion:\n" + prompt_response[len(prompt):])
+    data.append({
+            'losses': [round(l['adv_total'], 4) for l in losses],
+            'prompt': prompt,
+            'prompt_response': prompt_response[len(prompt):]
+        })
+    with open('/root/autodl-tmp/at/output.json', 'w', encoding='utf-8') as f:
+        json.dump(data, f, ensure_ascii=False, indent=4)
+
+
+
